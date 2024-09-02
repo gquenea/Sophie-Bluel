15396625b6e5933a908dfdc089d01f9836b7a9ef
unknown <quenea.gislain@gmail.com>
2024-09-02 17:38:37 +0200

    ended login and logout and first draft for modal

---
+++ FrontEnd/login.js
@@ -0,0 +1,29 @@
+const loginForm = document.getElementById("login-form");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+let token = "";
+
+loginForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const response = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value,
+    }),
+  });
+
+  if (response.status === 200) {
+    const setToken = await response.json();
+    if (typeof token === "string" && localStorage.getItem("token") === null) {
+      localStorage.setItem("token", setToken.token);
+      setTimeout(() => {
+        window.location.href = "./index.html";
+      }, 100);
+    }
+  } else {
+    const errorMessage = document.getElementById("error-message");
+    errorMessage.innerHTML = `Les identifiants que vous avez saisi sont incorrects`;
+  }
+});
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,4 +1,6 @@
 let allWorks = [];
+let allCategories = [];
+let ismodalOpened = false;
 
 // Get All Works
 fetch("http://localhost:5678/api/works")
@@ -10,6 +12,7 @@ fetch("http://localhost:5678/api/works")
   })
   .then((data) => {
     allWorks = data;
+    displayWorksOnModal(allWorks);
     displayWorks(allWorks);
   })
   .catch((error) => {
@@ -19,7 +22,7 @@ fetch("http://localhost:5678/api/works")
 // Function to display all works
 function displayWorks(works) {
   const worksGallery = document.getElementById("gallery");
-  worksGallery.innerHTML = ""; // Efface le contenu existant de la galerie
+  worksGallery.innerHTML = "";
 
   works.forEach((work) => {
     const workElement = document.createElement("figure");
@@ -33,12 +36,46 @@ function displayWorks(works) {
   });
 }
 
-// Filter works by category
-document.getElementById("filter-all").addEventListener("click", () => filterWorks("all"));
-document.getElementById("filter-objects").addEventListener("click", () => filterWorks(1));
-document.getElementById("filter-appartments").addEventListener("click", () => filterWorks(2));
-document.getElementById("filter-hotels").addEventListener("click", () => filterWorks(3));
+// Get All Categories
+fetch("http://localhost:5678/api/categories")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    allCategories = data;
+    displayFilter(allCategories);
+  })
+  .catch((error) => {
+    console.error("Erreur:", error);
+  });
 
+// Function to display all categories
+function displayFilter(categories) {
+  if (!localStorage.getItem("token")) {
+    const filters = document.getElementById("filters");
+
+    // Generate the all button
+    const allButton = document.createElement("button");
+    allButton.textContent = "Tous";
+    allButton.addEventListener("click", () => filterWorks("all"));
+    filters.appendChild(allButton);
+
+    // Generate others buttons
+    categories.forEach((category) => {
+      const categoryElement = document.createElement("button");
+      categoryElement.textContent = category.name;
+      categoryElement.setAttribute("data-category-id", category.id);
+      categoryElement.addEventListener("click", () => filterWorks(category.id));
+
+      filters.appendChild(categoryElement);
+    });
+  }
+}
+
+// Function to display works according to their categories
 function filterWorks(category) {
   if (category === "all") {
     displayWorks(allWorks);
@@ -46,4 +83,56 @@ function filterWorks(category) {
     const filteredWorks = allWorks.filter((work) => work.category.id === category);
     displayWorks(filteredWorks);
   }
+}
+
+// Function to display items only when user is logged in
+isLoggedDisplay();
+
+function isLoggedDisplay() {
+  if (localStorage.getItem("token")) {
+    // Display login or logout
+    const loginNavBtn = document.getElementById("login-nav-btn");
+    loginNavBtn.classList.add("hide-nav-btn");
+    const logoutNavBtn = document.getElementById("logout-nav-btn");
+    logoutNavBtn.addEventListener("click", () => logout());
+
+    const editBtn = document.getElementById("editWorks");
+    const editIcon = document.createElement("i");
+    editIcon.className = "fa-regular fa-pen-to-square";
+    const editSpan = document.createElement("span");
+    editSpan.textContent = "Modifier";
+
+    editSpan.addEventListener("click", () => openModal());
+
+    editBtn.appendChild(editIcon);
+    editBtn.appendChild(editSpan);
+  } else {
+    const logoutNavBtn = document.getElementById("logout-nav-btn");
+    logoutNavBtn.classList.add("hide-nav-btn");
+  }
+}
+
+function logout() {
+  localStorage.clear();
+}
+
+function openModal() {
+  ismodalOpened = true;
+}
+
+// Display all works on modal
+function displayWorksOnModal(works) {
+  const modalWorks = document.getElementById("modal-works");
+  modalWorks.innerHTML = "";
+
+  works.forEach((work) => {
+    const workElement = document.createElement("figure");
+
+    workElement.innerHTML = `
+      <img src="${work.imageUrl}" alt="${work.title}" />
+      <i id="delete" class="fa-solid fa-trash-can"></i>
+    `;
+
+    modalWorks.appendChild(workElement);
+  });
 }
